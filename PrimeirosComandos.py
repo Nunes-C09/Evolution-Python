0b9a8c8853685c03950bde33feb973b7e7445390
Nunes-C09 <104801598+Nunes-C09@users.noreply.github.com>
2023-06-02 14:23:24 -0300

    Add files via upload

---
+++ PrimeirosComandos.py
@@ -0,0 +1,10 @@
+"""
+nome = 'Kaike'
+idade = 18
+peso = 84.6
+print(nome, idade, peso)"""
+
+nome = input('Qual é o seu nome? ')
+idade = input('Quantos anos voce tem? ')
+peso = input('Quanto voce pesa? ')
+print(nome, idade, peso)
